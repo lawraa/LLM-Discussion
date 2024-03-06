e4016e5e76eb819d2cc9df5c68469d8ec62d5f5e
Lawrance <chenlawrance@gmail.com>
2024-03-07 02:40:43 +0800

    LeaderBoard Update

--- Evaluation/auto_grade_final.py
+++ Evaluation/auto_grade_final.py
@@ -180,6 +180,8 @@ def main():
     if args.output == 'y':
         mean_std_results = calculate_mean_std(total_results)
         output_csv_path = os.path.join(Path(__file__).parent, '..', 'Results', 'LeaderBoard', f'LeaderBoard-{args.task}.csv')
+        # output_csv_path is changed to /tmp2/LeaderBoard
+        # output_csv_path = f"/tmp2/LeaderBoard/LeaderBoard-{args.task}.csv"
         print(output_csv_path)
         write_results_to_csv(args.input_file, mean_std_results, output_csv_path, args.version)
         

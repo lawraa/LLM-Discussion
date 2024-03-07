b31bfcfcd4cb98f05898d156be46de2945cb83e7
Bai1026 <1132abc30@gmail.com>
2024-03-07 10:48:27 +0800

    update leader board

--- Results/LeaderBoard/filling.py
+++ Results/LeaderBoard/filling.py
@@ -3,8 +3,6 @@ import glob
 import json
 import numpy as np
 
-input_paths = glob.glob("./tmp/*")
-
 def calculate_mean_std(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
@@ -31,12 +29,6 @@ def calculate_mean_std(file_path):
                 if 'average_elaboration' in elaboration:
                     elaboration_scores.append(elaboration['average_elaboration'])
 
-    # averages = {
-    #     "fluency": {"mean": np.mean(fluency_scores), "std": np.std(fluency_scores)},
-    #     "flexibility": {"mean": np.mean(flexibility_scores), "std": np.std(flexibility_scores)},
-    #     "originality": {"mean": np.mean(originality_scores), "std": np.std(originality_scores)},
-    #     "elaboration": {"mean": np.mean(elaboration_scores), "std": np.std(elaboration_scores)}
-    # }
     averages = {
         "fluency": {"mean": round(np.mean(fluency_scores), 2), "std": round(np.std(fluency_scores), 2)},
         "flexibility": {"mean": round(np.mean(flexibility_scores), 2), "std": round(np.std(flexibility_scores), 2)},
@@ -46,22 +38,26 @@ def calculate_mean_std(file_path):
 
     return fluency_scores, flexibility_scores, originality_scores, elaboration_scores, averages
 
+
+input_paths = glob.glob("./tmp/*")
+
 for input_file_name in input_paths:
     data = []
     fluency_scores, flexibility_scores, originality_scores, elaboration_scores, averages = calculate_mean_std(input_file_name)
 
-    # input_file_name = input_file_name.split('_')[2:]
+    input_file_name = input_file_name.split('_')[1:]
+    # print(input_file_name)
 
-    # Task_Type = input_file_name[0]
-    # Subtask_Type = input_file_name[2]
+    Task_Type = input_file_name[0]
+    Subtask_Type = input_file_name[2]
 
-    # Agent = input_file_name[1]
+    Agent = input_file_name[1]
     
-    # version = 3.5
-    # if Agent == "single":
-    #     Agent = 1
-    #     rounds = 1
-    #     Agent_Description = f"gpt-{version}"
+    version = 3.5
+    if Agent == "single":
+        Agent = 1
+        rounds = 1
+        Agent_Description = f"gpt-{version}-turbo"
     # elif Agent == 'multi':
     #     Agent = input_file_name[3]
     #     rounds = input_file_name[4]
@@ -73,13 +69,13 @@ for input_file_name in input_paths:
 
     # prompt = input_file_name.split('_')[-6]
     # print(prompt)
-    Task_Type = 'AUT'
-    Subtask_Type = 'round-test'
-    Agent = 2
-    Agent_Description = 'gpt-3.5, gpt-3.5'
-    data_num = 10
+    # Task_Type = 'AUT'
+    # Subtask_Type = 'round-test'
+    # Agent = 2
+    # Agent_Description = 'gpt-3.5, gpt-3.5'
+    data_num = 100
     # rounds = 6
-    rounds = input_file_name.split('_')[-4]
+    # rounds = input_file_name.split('_')[-4]
     # print(rounds)
 
     # row = [Task_Type, f"{Subtask_Type}-{prompt}", Agent, Agent_Description, data_num, rounds, 
@@ -88,6 +84,26 @@ for input_file_name in input_paths:
 
     print(row)
 
-    with open("./unsorted-LeaderBoard.csv", mode="a", newline="") as file:
+    with open(f"./LeaderBoard-{Task_Type}.csv", mode="a", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(row)
+
+
+csv_file_path = './LeaderBoard-AUT.csv'
+sorted_csv_file_path = csv_file_path
+
+data = []
+with open(csv_file_path, mode='r', newline='') as file:
+    reader = csv.reader(file)
+    header = next(reader)  
+    for row in reader:
+        data.append(row)
+
+sorted_data = sorted(data, key=lambda x: (int(x[2]), int(x[4]), x[1], int(x[4]), x[5]))
+
+with open(sorted_csv_file_path, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(header)
+    writer.writerows(sorted_data)
+
+print(f'Data sorted by Task_Type and Data and saved to {sorted_csv_file_path}')
